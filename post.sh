d839058df91a60b0bea6b9a2d83e6d1b4e1996e9
Robert Faulk <robertfaulk@utexas.edu>
2017-05-02 16:48:06 -0500

    update post script for arbitrary cookies

--- post.sh
+++ post.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-# example POST request to the local server
-# adds the cookie (COOKIE, 12345) to github
-curl --data "url=github.com&cookie_name=COOKIE&cookie_value=12345" 127.0.0.1:5000/cookie_jar
+# example POST request script for sending to the local server
+
+if [ $# -ne 3 ]; then
+    (>&2 echo "Usage: ./$(basename $0) url cookie_name cookie_value")
+    exit 1
+fi
+
+curl --data "url=${1}&cookie_name=${2}&cookie_value=${3}" 127.0.0.1:5000/cookie_jar

6d2681ffdf28744212f688a4ae32a3f836d2e635
Robert Faulk <robertfaulk@utexas.edu>
2017-05-01 21:56:28 -0500

    fix post comment

--- post.sh
+++ post.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # example POST request to the local server
-# adds the cookie (COOKIE, 12345) to google.com
-curl --data "url=github.com&cookie_name=COOKIE&cookie_value=12346" 127.0.0.1:5000/cookie_jar
+# adds the cookie (COOKIE, 12345) to github
+curl --data "url=github.com&cookie_name=COOKIE&cookie_value=12345" 127.0.0.1:5000/cookie_jar
